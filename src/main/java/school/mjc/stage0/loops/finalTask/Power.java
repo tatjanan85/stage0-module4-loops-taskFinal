1d4d1879efab423e486b2de728d3a2478febd659
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-28 15:28:32 +0300

    feat: module4-loops final-task

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int powerOfNumber = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            powerOfNumber *= numberToPrint;
+        }
+        System.out.println(powerOfNumber);
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(10, 5);
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength * 2 - 1; j++) {
+                if (j <= cathetusLength){
+                    if (j < cathetusLength - i + 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(cathetusLength - j + 1);
+                    }
+                } else {
+                    if (j > cathetusLength + i -1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(j - cathetusLength + 1);
+                    }
+                }
+            }
+                System.out.println();
+        }
     }
 
     public static void main(String[] args) {
