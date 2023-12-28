e6ba0b9d728a69bba4145f6b9474b67b03acbeef
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-28 15:44:14 +0300

    fix: pyramid task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -4,20 +4,21 @@ public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
             for (int j = 1; j <= cathetusLength * 2 - 1; j++) {
-                if (j <= cathetusLength){
+                if (j <= cathetusLength) {
                     if (j < cathetusLength - i + 1) {
                         System.out.print(" ");
                     } else {
                         System.out.print(cathetusLength - j + 1);
                     }
                 } else {
-                    if (j > cathetusLength + i -1) {
+                    if (j > cathetusLength + i - 1) {
                         System.out.print(" ");
                     } else {
                         System.out.print(j - cathetusLength + 1);
                     }
                 }
             }
+            System.out.println();
         }
     }
 
